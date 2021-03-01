a52bc87f0dfb3d0aa098ecf483015a1ffb1fcdc8
MaksimKlanovets <klanovetsm@gmail.com>
2021-03-01 22:42:21 +0200

    add new files

---
+++ SF_14_t9/Autocomplete.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+#define  ALPHABET_SIZE  26
+
+
+// Структура узела дерева 
+struct TrieNode
+{
+    struct TrieNode* children[ALPHABET_SIZE];
+    // isEndOfWord - true, если ключ является концом слова
+    bool isEndOfWord;
+};
+
+TrieNode* getNewNode(void);
+void insert(TrieNode*, string);
+bool search(TrieNode*, string);
+bool isEmpty(TrieNode*);
+TrieNode* remove(TrieNode*, string, int depth = 0);
+void findMinPrefixes(TrieNode*, char[], int, string&);
+void print(TrieNode*, int depth = 0);
